3b09c6d7ca5f47b0ad9bd6f8e3b437cbb2ff5b86
Arslan <ali.arslan319@gmail.com>
2021-10-08 22:56:01 -0400

    resolve issue to return data instead of the promise of that data

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const http = new EasyHTTP()
 
 // getting users 
-const users = http.get('https://jsonplaceholder.typicode.com/users')
+// line 5 will only return the promise, not the data itself 
+// const users = http.get('https://jsonplaceholder.typicode.com/users')
 
-console.log(users)
+http.get('https://jsonplaceholder.typicode.com/users')
+.then(data => console.log(data))
+.catch(error => console.log(error))
